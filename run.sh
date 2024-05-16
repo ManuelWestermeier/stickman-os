9aee7e144c66a522df1a162598ca53dad548fdee
ManuelWestermeier <westermeier111@gmail.com>
2024-05-16 16:36:50 +0200

    first commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-nasm -f bin stickman.asm -o stickman.bin
+nasm -f bin index.asm -o stickman.bin
 dd if=/dev/zero of=floppy.img bs=512 count=2880
 dd if=stickman.bin of=floppy.img conv=notrunc
 qemu-system-x86_64 -fda floppy.img
